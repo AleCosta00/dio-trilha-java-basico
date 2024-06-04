4845ee0c40cee8aea2a83763cd8031e41a3ff53d
Alexandre Costa <36090502+AleCosta00@users.noreply.github.com>
2024-06-04 16:47:24 -0300

    Exemplos de tipos primitivos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //a classe String representa texto na aplicação
+
+        //exemplo de usabilidade de String
+        String meuNome = "Alexandre Costa";
+
+        //exemplo de usabilidade de variáveis do tipo inteiro
+        int idade = 27;
+
+        //exemplo de usabilidade de variáveis do tipo double
+        double salarioMinimo = 2500;
+
+        //exemplo de usabilidade de variáveis do tipo short
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
     }
 }
