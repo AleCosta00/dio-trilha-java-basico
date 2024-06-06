f1f476f242f1146a141c68e923ec7031de5cf875
Alexandre Costa <36090502+AleCosta00@users.noreply.github.com>
2024-06-06 16:35:16 -0300

    Operadores Aritméticos.

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem " + "Java";
+        System.err.println(nomeCompleto);
+
+        //qual o resultado das expressões abaixo?
+
+        String concatenacao = "?";
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+    }
+}
