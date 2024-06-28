4c123267c47e70da5655353fc4292a0bb237e308
Alexandre Costa <36090502+AleCosta00@users.noreply.github.com>
2024-06-28 16:00:42 -0300

    Desafio de Projeto - Controle de Fluxo

--- tipos-variaveis/src/OperadoresLogicos.java
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -1,7 +1,7 @@
 public class OperadoresLogicos {
     public static void main(String[] args) {
         boolean condicao1 = true;
-        boolean condicao2 = false;
+        boolean condicao2 = false ;
 
         if (condicao1 && condicao2) {
             System.out.println("As duas condições são verdadeiras");
